0a841673d935099df28a167cc657623ffc160201
mirroflower <xywxd666@126.com>
2022-11-01 08:46:12 +0800

    保存代码

---
+++ src/views/user/HelloWoldView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>{{ title }}</div>
+  </template>
+  
+  <script>
+  let app
+  export default {
+    name: 'Hello World',
+    data() {
+      return {
+        title: 'Hello World',
+      }
+    },
+    computed: {},
+    methods: {},
+    created() {
+      app = this
+      console.log(app.title)
+    },
+  }
+  </script>
